977353bb0caff6ad41fe10d4d4781a9fa0e9fec0
Sara <rrrrrah@gmail.com>
2014-08-12 15:03:00 -0400

    added exercise 2

---
+++ exercise2.rb
@@ -0,0 +1,15 @@
+# Question 1
+meal = 55
+tip = meal / 5
+puts "A good tip for a #{meal} dollar meal is #{tip} dollars."
+
+#Question 2
+puts "The street number here is " + 220.to_s
+
+#Question 3
+number1 = 45628
+number2 = 7839
+puts "#{number1 * number2}"
+
+#Question 4
+puts "(true && false) || (false && true) || !(false && false) - TRUE!"
