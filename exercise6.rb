fc7291fe04112958e31d3933fc6dd321f61ec466
Sara <rrrrrah@gmail.com>
2014-08-13 16:25:38 -0400

    Added exercise 6

---
+++ exercise6.rb
@@ -0,0 +1,53 @@
+#Part 1
+grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
+
+def print_list(list)
+  list.each do |item|
+    puts "* " + item
+  end
+end
+
+print_list(grocery_list)
+
+#Part 2
+grocery_list.push("rice")
+print_list(grocery_list)
+
+#Part 3
+puts grocery_list.length
+
+#Part 4
+if grocery_list.include?"bananas"
+	puts "You need to pick up bananas."
+else puts "You don't need to pick up bananas today."
+end
+
+#Part 5
+puts grocery_list[1]
+
+#Part 6
+puts grocery_list.sort
+
+grocery_list2 = ["apples", "carrots", "rice", "salmon", "toilet paper"]
+
+def print_list(list)
+  list.each do |item|
+  	puts "* " + item
+  end
+ end
+
+print_list(grocery_list2)
+
+#Part 7
+grocery_list2.delete"salmon"
+print_list(grocery_list2)
+
+
+
+
+
+
+
+
+
+
