03bb268feec41d25065d58159745a30ce8ded9f0
Sara <rrrrrah@gmail.com>
2014-08-13 16:46:59 -0400

    Added updated exercise 6

--- exercise6.rb
+++ exercise6.rb
@@ -1,11 +1,11 @@
-#Part 1
-grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
-
 def print_list(list)
   list.each do |item|
     puts "* " + item
   end
 end
+
+#Part 1
+grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
 
 print_list(grocery_list)
 
@@ -17,37 +17,19 @@ print_list(grocery_list)
 puts grocery_list.length
 
 #Part 4
-if grocery_list.include?"bananas"
-	puts "You need to pick up bananas."
-else puts "You don't need to pick up bananas today."
+if grocery_list.include? "bananas"
+  puts "You need to pick up bananas."
+else 
+  puts "You don't need to pick up bananas today."
 end
 
 #Part 5
 puts grocery_list[1]
 
 #Part 6
-puts grocery_list.sort
-
-grocery_list2 = ["apples", "carrots", "rice", "salmon", "toilet paper"]
-
-def print_list(list)
-  list.each do |item|
-  	puts "* " + item
-  end
- end
-
-print_list(grocery_list2)
+grocery_list = grocery_list.sort
+print_list(grocery_list)
 
 #Part 7
-grocery_list2.delete"salmon"
-print_list(grocery_list2)
-
-
-
-
-
-
-
-
-
-
+grocery_list.delete "salmon"
+print_list(grocery_list)
